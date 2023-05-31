1bdd625c1721fe8c55543c1c48723a1a2f965055
Darkpfeffer <darkpfeffer97@gmail.com>
2023-05-31 14:23:12 +0200

    letter-view mobile responsiveness done

--- src/components/letter-view/letter-view.jsx
+++ src/components/letter-view/letter-view.jsx
@@ -3,18 +3,33 @@ import "./letter-view.scss"
 export const LetterView = () => {
     return (
         <div className="letter">
-            <p className="letter-header">Kedves Családunk és Barátaink!</p>
-            <p className="letter-text">Több éves próbaidő után Zoli úgy döntött, az elkövetkezendő évtizedeket is Kincsővel kívánja tölteni, így megkérte a kezét.<br/>
-            Kincső szintúgy meggyőződött arról, hogy neki Zolival a legjobb, ezért a gyümölcsöző folytatás érdekében igent mondott.<br/>
-            A sikeres eljegyzést természetesen menyegzővel koronázzuk.<br/>
-            Idén novemberben nem csak négyszemközt, hanem szem- és fültanúk előtt, hivatalosan is igent mondunk egymásnak.<br/>
-            Házasságunk első óráiban szeretnénk jóakaratú és jókedvű társaságban ünnepelni.<br/>
-            Ezen sorok olvasása azt jelenti, egyike vagy azoknak, akiknek jelenléte megszépítené esküvőnk életre szóló emlékét.<br/>
-            Fogadd hát jószívvel szeretetteljes meghívásunkat a házasságkötésünkre, és az azt követő ünnepi lakomára!<br/>
-            <br/>
-
-            Szeretettel:<br/>
-            A Menyasszonytól és a Vőlegénytől</p>
+            <p className="letter-header">
+                Kedves Családunk <br className="mobile"></br>
+                és Barátaink!
+            </p>
+            <p className="letter-text">
+                Több éves próbaidő után Zoli úgy döntött, <br className="mobile"></br> 
+                az elkövetkezendő évtizedeket is Kincsővel <br className="mobile"></br>
+                kívánja tölteni, így megkérte a kezét.<br/>
+                Kincső szintúgy meggyőződött arról, hogy <br className="mobile"></br>
+                neki Zolival a legjobb, ezért a gyümölcsöző <br className="mobile"></br>
+                folytatás érdekében igent mondott.<br/>
+                A sikeres eljegyzést természetesen <br className="mobile"></br>
+                menyegzővel koronázzuk.<br/>
+                Idén novemberben nem csak négyszemközt, <br className="mobile"></br>
+                hanem szem- és fültanúk előtt, <br className="mobile"></br>
+                hivatalosan is igent mondunk egymásnak.<br/>
+                Házasságunk első óráiban szeretnénk <br className="mobile"></br>
+                jóakaratú és jókedvű társaságban ünnepelni.<br/>
+                Ezen sorok olvasása azt jelenti, egyike vagy <br className="mobile"></br>
+                azoknak, akiknek jelenléte megszépítené <br className="mobile"></br>
+                esküvőnk életre szóló emlékét.<br/>
+                Fogadd hát jószívvel szeretetteljes <br className="mobile"></br>
+                meghívásunkat a házasságkötésünkre, <br className="mobile"></br>
+                és az azt követő ünnepi lakomára!<br/>
+                <br/>
+                Szeretettel:<br/>
+                A Menyasszonytól és a Vőlegénytől</p>
         </div>
     )
 }
