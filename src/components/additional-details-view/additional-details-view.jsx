81a7d60548942bd8bfa6f3a2677b7664d9caaa71
Darkpfeffer <darkpfeffer97@gmail.com>
2023-06-03 18:52:05 +0200

    additional details (tablet responsive)

--- src/components/additional-details-view/additional-details-view.jsx
+++ src/components/additional-details-view/additional-details-view.jsx
@@ -5,27 +5,47 @@ export const AdditionalDetailsView = () => {
         <div className="additional-details">
             <p className="additional-header">Lesz - Nem lesz</p>
             <p className="additional-text">
-                Nem lesz: papíralapú meghívó, rizsszórás, menyasszony/menyecske/vejecsketánc, táncos mulatság.<br/>
-                Lesz: koszorúslányok királykékben, saját fogadalom, néhány örömkönny, kvíz és más meglepik!
+                Nem lesz: papíralapú meghívó, rizsszórás, <br className="mobile"/>
+                menyasszony/menyecske/vejecsketánc, <br className="mobile"/>
+                táncos mulatság.<br className="desktop mobile"/>
+                Lesz: koszorúslányok királykékben, <br className="mobile"/>
+                saját fogadalom, néhány örömkönny, <br className="mobile"/>
+                kvíz és más meglepik!
             </p>
             <p className="additional-header">Dress Code</p>
             <p className="additional-text">
-                Hogy bárki messziről kiszúrhassa az összeesküvőket a tömegben, tisztelettel arra kérünk minden Hölgyet és Urat,<br/>
-                hogy ne fehéret vagy feketét viseljenek. Válasszák inkább a szürke 50 árnyalatát, vagy a szivárvány bármely színét!
+                Hogy bárki messziről kiszúrhassa az <br className="mobile"/>
+                összeesküvőket a tömegben, tisztelettel arra <br className="mobile"/>
+                kérünk minden Hölgyet és Urat,<br className="desktop"/>
+                hogy ne <br className="mobile"/>
+                fehéret vagy feketét viseljenek. Válasszák <br className="mobile"/>
+                inkább a szürke 50 árnyalatát, vagy a <br className="mobile"/>
+                szivárvány bármely színét!
             </p>
             <p className="additional-header">Megörökített pillanatok</p>
             <p className="additional-text">
-                Profi fotók készülnek majd az Esküvőn és a Nászúton, amiket kérésre megosztunk Vendégeinkkel.<br/>
-                Ennélfogva arra bátorítjuk Násznépünket, fotózás és videózás helyett éljenek a pillanatoknak!<br/>
-                Lelkes Facebooker Vendégeinket tisztelettel kérjük, tartózkodjanak az intim pillanatok posztolásától.
+                Profi fotók készülnek majd az Esküvőn és a <br className="mobile"/>
+                Nászúton, amiket kérésre megosztunk <br className="mobile"/>
+                Vendégeinkkel.<br className="desktop"/>
+                Ennélfogva arra bátorítjuk <br className="mobile"/>
+                Násznépünket, fotózás és videózás helyett <br className="mobile"/>
+                éljenek a pillanatoknak!<br className="desktop"/>
+                Lelkes Facebooker <br className="mobile"/>
+                Vendégeinket tisztelettel kérjük, <br className="mobile"/>
+                tartózkodjanak az intim pillanatok <br className="mobile"/>
+                posztolásától.
             </p>
             <p className="additional-header">Nászajándék</p>
             <p className="additional-text">
-                A leendő friss házasok irulva-pirulva, hálás szívvel fogják fogadni a borítékba rejtett, papíralapú nászajándékokat.
+                A leendő friss házasok irulva-pirulva, hálás <br className="mobile"/>
+                szívvel fogják fogadni a borítékba rejtett, <br className="mobile"/>
+                papíralapú nászajándékokat.
             </p>
             <p className="additional-header">Visszajelzés</p>
             <p className="additional-text">
-            Midőn megszületett a döntés a részvételről, és a dátum is bekerült a naptárba, kérjük üzenetben visszajelezni!
+            Midőn megszületett a döntés a részvételről, <br className="mobile"/>
+            és a dátum is bekerült a naptárba, kérjük <br className="mobile"/>
+            üzenetben visszajelezni!
             </p>
         </div>
     )
