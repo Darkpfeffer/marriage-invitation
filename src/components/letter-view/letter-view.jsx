8f2c30635a54a2a3ed84e4f012415415520d8dab
Darkpfeffer <darkpfeffer97@gmail.com>
2023-06-06 22:43:22 +0200

    letter view is responsive on tablet

--- src/components/letter-view/letter-view.jsx
+++ src/components/letter-view/letter-view.jsx
@@ -10,23 +10,23 @@ export const LetterView = () => {
             <p className="letter-text">
                 Több éves próbaidő után Zoli úgy döntött, <br className="mobile"></br> 
                 az elkövetkezendő évtizedeket is Kincsővel <br className="mobile"></br>
-                kívánja tölteni, így megkérte a kezét.<br/>
+                kívánja tölteni, így megkérte a kezét. <br className="mobile desktop"/>
                 Kincső szintúgy meggyőződött arról, hogy <br className="mobile"></br>
                 neki Zolival a legjobb, ezért a gyümölcsöző <br className="mobile"></br>
-                folytatás érdekében igent mondott.<br/>
+                folytatás érdekében igent mondott. <br className="mobile desktop"/>
                 A sikeres eljegyzést természetesen <br className="mobile"></br>
-                menyegzővel koronázzuk.<br/>
+                menyegzővel koronázzuk. <br className="mobile desktop"/>
                 Idén novemberben nem csak négyszemközt, <br className="mobile"></br>
                 hanem szem- és fültanúk előtt, <br className="mobile"></br>
-                hivatalosan is igent mondunk egymásnak.<br/>
+                hivatalosan is igent mondunk egymásnak. <br className="mobile desktop"/>
                 Házasságunk első óráiban szeretnénk <br className="mobile"></br>
-                jóakaratú és jókedvű társaságban ünnepelni.<br/>
+                jóakaratú és jókedvű társaságban ünnepelni. <br className="mobile desktop"/>
                 Ezen sorok olvasása azt jelenti, egyike vagy <br className="mobile"></br>
                 azoknak, akiknek jelenléte megszépítené <br className="mobile"></br>
-                esküvőnk életre szóló emlékét.<br/>
+                esküvőnk életre szóló emlékét. <br className="mobile desktop"/>
                 Fogadd hát jószívvel szeretetteljes <br className="mobile"></br>
                 meghívásunkat a házasságkötésünkre, <br className="mobile"></br>
-                és az azt követő ünnepi lakomára!<br/>
+                és az azt követő ünnepi lakomára! <br/>
                 <br/>
                 Szeretettel:<br/>
                 A Menyasszonytól és a Vőlegénytől</p>
