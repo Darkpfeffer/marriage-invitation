fc37385dbe6337d8f5b00eb18369fbf0cd4d71c8
Darkpfeffer <darkpfeffer97@gmail.com>
2023-06-08 17:41:33 +0200

    additional details page is responsive on mobile

--- src/components/about-us-view/about-us-view.jsx
+++ src/components/about-us-view/about-us-view.jsx
@@ -3,30 +3,57 @@ import WikiImage from "../../img/Wiki.png"
 import "./about-us-view.scss"
 export const AboutUsView = () => {
     return (
-        <div className="about-us-content">
-            <img src={WikiImage} rel="wiki image" className="about-us__img"></img>
-            <h3 className="about-us__title"><span className="about-us__title--capital">K</span>incső és Zoltán története</h3>
-            <p>
-                <span className="about-us__year">2019</span>. április 19-én ismerkedtünk meg, Németországban, mikor közös ismerősünk bemutatott minket egymásnak munkahelyünkön. A nyilvánvaló nehézségek ellenére, hogy tudtuk, távkapcsolat vár ránk, összejöttünk. Júniusban már össze is költöztünk, de szeptembertől Kincső Budapestre ment tanulni, és csak pár hetente tudtunk találkozni. Az egyik ilyen találkozás során vettük meg első közös kiskedvencünket, egy Vénusz légycsapóját, ami egy rovaremésztő növény.
-            </p>
-            <p>
-                <span className="about-us__year">2020</span> márciusában, mikor kirobbant a világjárvány, távoktatást vette kezdetét, úgyhogy újra egy fedél alatt élhettünk. Nem sokkal később, első évfordulónkon, megvettük második közös kiskedvencünket is, egy tengerimalacot, akit Morzsának vagy Milimalinak szólítottunk. A nyáron még egy kis utazást is sikerült beiktatunk, Amszterdamba látogattunk, és a Balatonon üdültünk. Aztán újra eljött az ősz, és mivel csitult a járvány, hibrid oktatás vette kezdetét, ami azt jelentette, Kincső újra elment Budapestre tanulni, és a legnehezebb hónapok köszöntöttek ránk, mert egyáltalán nem tudtunk találkozni a karanténszabályok miatt. Ezek a hónapok arra tanítottak minket, értékeljük azt is, ami olyan természetesnek tűnik: hogy az ember együtt él szerelmével, találkozhat vele nap mint nap, és egy utazás vége nem jelenti az együtt töltött idő végét is.
-            </p>
-            <p>
-                <span className="about-us__year">2021</span> év elején a távkapcsolatok korszaka számunkra véget ért, egyszer és mindenkorra, mivel visszavezették a távoktatást. Február 4-én családunk bővült, kutyánk, Leszter megvételével. A kölyökkora sok kihívást tartogatott, igencsak felforgatta életünket, de mára már egy kedves kutyus lett. A nyáron beiktattunk egy kis tengerparti pihenést, Görögországba, Míknosz szigetére repültünk, ez volt Zoli élete első repülőútja.
-            </p>
-            <p>
-                <span className="about-us__year">2022</span> februárjában pedig igazán különleges élményben volt részünk, egy maldív-szigeteki álomutazás formájában. Tavasszal autótávolságra lévő városokba látogattunk el, Berlinbe és Prágába. Ősszel mindketten karrierváltó tanfolyamba fogtunk, ami igencsak átformálta hétköznapjainkat és jövőnket. A tél beköszöntével elveszítettük első két kiskedvencünket, ámde nem csak rossz dolgok értek minket. A téli naprofulón, december 21-én, mikor Kincső hazaért, egy virágcsokor és egy üzenet fogadta Zolitól. Meglepetésrandit szervezett egy olasz étterembe, és megtörtént a várva várt eljegyzés.
-            </p>
-            <p>
-                <span className="about-us__year">2023</span>-ban vár ránk az esküvő, a nászút, és még ki tudja mit tartogat az év. Reméljük, a jövő évtől kezdve pedig nagyobb terveinket is egy-kettőre valóra váltjuk!
-            </p>
-            <p className="about-us__quote">
-                <span className="about-us__quote--capital">A</span>kik ketten együtt járják az élet sokszínű útját, kevésbé fáradnak el, édesebben pihennek, és vidámabban vándorolnak tovább, mint a társtalan utasok. Örömük megkétszereződik, mert ketten örülnek neki,
-                bánatuk feleakkorává válik, mert ketten hordozzák, terheik könnyebbek,
-                mert ketten viselik, hitük erősebb, mert ketten hiszik.
-            </p>
-            <h4 className="about-us__thanks">Köszönünk minden kis és nagy segítséget idáig vezető utunkon és az esküvőszervezésben! </h4>
-        </div>
+        <>
+            <div className="about-us-content desktop">
+                <img src={WikiImage} rel="wiki image" className="about-us__img"></img>
+                <h3 className="about-us__title"><span className="about-us__title--capital">K</span>incső és Zoltán története</h3>
+                <p>
+                    <span className="about-us__year">2019</span>. április 19-én ismerkedtünk meg, Németországban, mikor közös ismerősünk bemutatott minket egymásnak munkahelyünkön. A nyilvánvaló nehézségek ellenére, hogy tudtuk, távkapcsolat vár ránk, összejöttünk. Júniusban már össze is költöztünk, de szeptembertől Kincső Budapestre ment tanulni, és csak pár hetente tudtunk találkozni. Az egyik ilyen találkozás során vettük meg első közös kiskedvencünket, egy Vénusz légycsapóját, ami egy rovaremésztő növény.
+                </p>
+                <p>
+                    <span className="about-us__year">2020</span> márciusában, mikor kirobbant a világjárvány, távoktatást vette kezdetét, úgyhogy újra egy fedél alatt élhettünk. Nem sokkal később, első évfordulónkon, megvettük második közös kiskedvencünket is, egy tengerimalacot, akit Morzsának vagy Milimalinak szólítottunk. A nyáron még egy kis utazást is sikerült beiktatunk, Amszterdamba látogattunk, és a Balatonon üdültünk. Aztán újra eljött az ősz, és mivel csitult a járvány, hibrid oktatás vette kezdetét, ami azt jelentette, Kincső újra elment Budapestre tanulni, és a legnehezebb hónapok köszöntöttek ránk, mert egyáltalán nem tudtunk találkozni a karanténszabályok miatt. Ezek a hónapok arra tanítottak minket, értékeljük azt is, ami olyan természetesnek tűnik: hogy az ember együtt él szerelmével, találkozhat vele nap mint nap, és egy utazás vége nem jelenti az együtt töltött idő végét is.
+                </p>
+                <p>
+                    <span className="about-us__year">2021</span> év elején a távkapcsolatok korszaka számunkra véget ért, egyszer és mindenkorra, mivel visszavezették a távoktatást. Február 4-én családunk bővült, kutyánk, Leszter megvételével. A kölyökkora sok kihívást tartogatott, igencsak felforgatta életünket, de mára már egy kedves kutyus lett. A nyáron beiktattunk egy kis tengerparti pihenést, Görögországba, Míknosz szigetére repültünk, ez volt Zoli élete első repülőútja.
+                </p>
+                <p>
+                    <span className="about-us__year">2022</span> februárjában pedig igazán különleges élményben volt részünk, egy maldív-szigeteki álomutazás formájában. Tavasszal autótávolságra lévő városokba látogattunk el, Berlinbe és Prágába. Ősszel mindketten karrierváltó tanfolyamba fogtunk, ami igencsak átformálta hétköznapjainkat és jövőnket. A tél beköszöntével elveszítettük első két kiskedvencünket, ámde nem csak rossz dolgok értek minket. A téli naprofulón, december 21-én, mikor Kincső hazaért, egy virágcsokor és egy üzenet fogadta Zolitól. Meglepetésrandit szervezett egy olasz étterembe, és megtörtént a várva várt eljegyzés.
+                </p>
+                <p>
+                    <span className="about-us__year">2023</span>-ban vár ránk az esküvő, a nászút, és még ki tudja mit tartogat az év. Reméljük, a jövő évtől kezdve pedig nagyobb terveinket is egy-kettőre valóra váltjuk!
+                </p>
+                <p className="about-us__quote">
+                    <span className="about-us__quote--capital">A</span>kik ketten együtt járják az élet sokszínű útját, kevésbé fáradnak el, édesebben pihennek, és vidámabban vándorolnak tovább, mint a társtalan utasok. Örömük megkétszereződik, mert ketten örülnek neki,
+                    bánatuk feleakkorává válik, mert ketten hordozzák, terheik könnyebbek,
+                    mert ketten viselik, hitük erősebb, mert ketten hiszik.
+                </p>
+                <h4 className="about-us__thanks">Köszönünk minden kis és nagy segítséget idáig vezető utunkon és az esküvőszervezésben! </h4>
+            </div>
+            <div className="about-us-content mobile">
+                <img src={WikiImage} rel="wiki image" className="about-us__img"></img>
+                <h4 className="about-us__title"><span className="about-us__title--capital">K</span>incső és Zoltán története</h4>
+                <p>
+                    <span className="about-us__year">2019</span>. április 19-én ismerkedtünk meg, Németországban, mikor közös ismerősünk bemutatott minket egymásnak munkahelyünkön. A nyilvánvaló nehézségek ellenére, hogy tudtuk, távkapcsolat vár ránk, összejöttünk. Júniusban már össze is költöztünk, de szeptembertől Kincső Budapestre ment tanulni, és csak pár hetente tudtunk találkozni. Az egyik ilyen találkozás során vettük meg első közös kiskedvencünket, egy Vénusz légycsapóját, ami egy rovaremésztő növény.
+                </p>
+                <p>
+                    <span className="about-us__year">2020</span> márciusában, mikor kirobbant a világjárvány, távoktatást vette kezdetét, úgyhogy újra egy fedél alatt élhettünk. Nem sokkal később, első évfordulónkon, megvettük második közös kiskedvencünket is, egy tengerimalacot, akit Morzsának vagy Milimalinak szólítottunk. A nyáron még egy kis utazást is sikerült beiktatunk, Amszterdamba látogattunk, és a Balatonon üdültünk. Aztán újra eljött az ősz, és mivel csitult a járvány, hibrid oktatás vette kezdetét, ami azt jelentette, Kincső újra elment Budapestre tanulni, és a legnehezebb hónapok köszöntöttek ránk, mert egyáltalán nem tudtunk találkozni a karanténszabályok miatt. Ezek a hónapok arra tanítottak minket, értékeljük azt is, ami olyan természetesnek tűnik: hogy az ember együtt él szerelmével, találkozhat vele nap mint nap, és egy utazás vége nem jelenti az együtt töltött idő végét is.
+                </p>
+                <p>
+                    <span className="about-us__year">2021</span> év elején a távkapcsolatok korszaka számunkra véget ért, egyszer és mindenkorra, mivel visszavezették a távoktatást. Február 4-én családunk bővült, kutyánk, Leszter megvételével. A kölyökkora sok kihívást tartogatott, igencsak felforgatta életünket, de mára már egy kedves kutyus lett. A nyáron beiktattunk egy kis tengerparti pihenést, Görögországba, Míknosz szigetére repültünk, ez volt Zoli élete első repülőútja.
+                </p>
+                <p>
+                    <span className="about-us__year">2022</span> februárjában pedig igazán különleges élményben volt részünk, egy maldív-szigeteki álomutazás formájában. Tavasszal autótávolságra lévő városokba látogattunk el, Berlinbe és Prágába. Ősszel mindketten karrierváltó tanfolyamba fogtunk, ami igencsak átformálta hétköznapjainkat és jövőnket. A tél beköszöntével elveszítettük első két kiskedvencünket, ámde nem csak rossz dolgok értek minket. A téli naprofulón, december 21-én, mikor Kincső hazaért, egy virágcsokor és egy üzenet fogadta Zolitól. Meglepetésrandit szervezett egy olasz étterembe, és megtörtént a várva várt eljegyzés.
+                </p>
+                <p>
+                    <span className="about-us__year">2023</span>-ban vár ránk az esküvő, a nászút, és még ki tudja mit tartogat az év. Reméljük, a jövő évtől kezdve pedig nagyobb terveinket is egy-kettőre valóra váltjuk!
+                </p>
+                <p className="about-us__quote">
+                    <span className="about-us__quote--capital">A</span>kik ketten együtt járják az élet sokszínű útját, kevésbé fáradnak el, édesebben pihennek, és vidámabban vándorolnak tovább, mint a társtalan utasok. Örömük megkétszereződik, mert ketten örülnek neki,
+                    bánatuk feleakkorává válik, mert ketten hordozzák, terheik könnyebbek,
+                    mert ketten viselik, hitük erősebb, mert ketten hiszik.
+                </p>
+                <h5 className="about-us__thanks">Köszönünk minden kis és nagy segítséget idáig vezető utunkon és az esküvőszervezésben! </h5>
+            </div>
+        </>
     )
 }
--- src/components/letter-view/letter-view.jsx
+++ src/components/letter-view/letter-view.jsx
@@ -2,34 +2,65 @@ import "./letter-view.scss"
 
 export const LetterView = () => {
     return (
-        <div className="letter">
-            <h2 className="letter-header">
-                Kedves Családunk <br className="mobile"></br>
-                és Barátaink!
-            </h2>
-            <p className="letter-text">
-                Több éves próbaidő után Zoli úgy döntött, <br className="mobile"></br> 
-                az elkövetkezendő évtizedeket is Kincsővel <br className="mobile"></br>
-                kívánja tölteni, így megkérte a kezét. <br className="mobile desktop"/>
-                Kincső szintúgy meggyőződött arról, hogy <br className="mobile"></br>
-                neki Zolival a legjobb, ezért a gyümölcsöző <br className="mobile"></br>
-                folytatás érdekében igent mondott. <br className="mobile desktop"/>
-                A sikeres eljegyzést természetesen <br className="mobile"></br>
-                menyegzővel koronázzuk. <br className="mobile desktop"/>
-                Idén novemberben nem csak négyszemközt, <br className="mobile"></br>
-                hanem szem- és fültanúk előtt, <br className="mobile"></br>
-                hivatalosan is igent mondunk egymásnak. <br className="mobile desktop"/>
-                Házasságunk első óráiban szeretnénk <br className="mobile"></br>
-                jóakaratú és jókedvű társaságban ünnepelni. <br className="mobile desktop"/>
-                Ezen sorok olvasása azt jelenti, egyike vagy <br className="mobile"></br>
-                azoknak, akiknek jelenléte megszépítené <br className="mobile"></br>
-                esküvőnk életre szóló emlékét. <br className="mobile desktop"/>
-                Fogadd hát jószívvel szeretetteljes <br className="mobile"></br>
-                meghívásunkat a házasságkötésünkre, <br className="mobile"></br>
-                és az azt követő ünnepi lakomára! <br/>
-                <br/>
-                Szeretettel:<br/>
-                A Menyasszonytól és a Vőlegénytől</p>
-        </div>
+        <>
+            <div className="letter desktop tablet">
+                <h2 className="letter-header">
+                    Kedves Családunk <br className="mobile"></br>
+                    és Barátaink!
+                </h2>
+                <p className="letter-text">
+                    Több éves próbaidő után Zoli úgy döntött, <br className="mobile"></br> 
+                    az elkövetkezendő évtizedeket is Kincsővel <br className="mobile"></br>
+                    kívánja tölteni, így megkérte a kezét. <br className="mobile desktop"/>
+                    Kincső szintúgy meggyőződött arról, hogy <br className="mobile"></br>
+                    neki Zolival a legjobb, ezért a gyümölcsöző <br className="mobile"></br>
+                    folytatás érdekében igent mondott. <br className="mobile desktop"/>
+                    A sikeres eljegyzést természetesen <br className="mobile"></br>
+                    menyegzővel koronázzuk. <br className="mobile desktop"/>
+                    Idén novemberben nem csak négyszemközt, <br className="mobile"></br>
+                    hanem szem- és fültanúk előtt, <br className="mobile"></br>
+                    hivatalosan is igent mondunk egymásnak. <br className="mobile desktop"/>
+                    Házasságunk első óráiban szeretnénk <br className="mobile"></br>
+                    jóakaratú és jókedvű társaságban ünnepelni. <br className="mobile desktop"/>
+                    Ezen sorok olvasása azt jelenti, egyike vagy <br className="mobile"></br>
+                    azoknak, akiknek jelenléte megszépítené <br className="mobile"></br>
+                    esküvőnk életre szóló emlékét. <br className="mobile desktop"/>
+                    Fogadd hát jószívvel szeretetteljes <br className="mobile"></br>
+                    meghívásunkat a házasságkötésünkre, <br className="mobile"></br>
+                    és az azt követő ünnepi lakomára! <br/>
+                    <br/>
+                    Szeretettel:<br/>
+                    A Menyasszonytól és a Vőlegénytől</p>
+            </div>
+            <div className="letter mobile">
+                <h3 className="letter-header">
+                    Kedves Családunk <br className="mobile"></br>
+                    és Barátaink!
+                </h3>
+                <p className="letter-text">
+                    Több éves próbaidő után Zoli úgy döntött, <br className="mobile"></br> 
+                    az elkövetkezendő évtizedeket is Kincsővel <br className="mobile"></br>
+                    kívánja tölteni, így megkérte a kezét. <br className="mobile desktop"/>
+                    Kincső szintúgy meggyőződött arról, hogy <br className="mobile"></br>
+                    neki Zolival a legjobb, ezért a gyümölcsöző <br className="mobile"></br>
+                    folytatás érdekében igent mondott. <br className="mobile desktop"/>
+                    A sikeres eljegyzést természetesen <br className="mobile"></br>
+                    menyegzővel koronázzuk. <br className="mobile desktop"/>
+                    Idén novemberben nem csak négyszemközt, <br className="mobile"></br>
+                    hanem szem- és fültanúk előtt, <br className="mobile"></br>
+                    hivatalosan is igent mondunk egymásnak. <br className="mobile desktop"/>
+                    Házasságunk első óráiban szeretnénk <br className="mobile"></br>
+                    jóakaratú és jókedvű társaságban ünnepelni. <br className="mobile desktop"/>
+                    Ezen sorok olvasása azt jelenti, egyike vagy <br className="mobile"></br>
+                    azoknak, akiknek jelenléte megszépítené <br className="mobile"></br>
+                    esküvőnk életre szóló emlékét. <br className="mobile desktop"/>
+                    Fogadd hát jószívvel szeretetteljes <br className="mobile"></br>
+                    meghívásunkat a házasságkötésünkre, <br className="mobile"></br>
+                    és az azt követő ünnepi lakomára! <br/>
+                    <br/>
+                    Szeretettel:<br/>
+                    A Menyasszonytól és a Vőlegénytől</p>
+            </div>
+        </>
     )
 }
