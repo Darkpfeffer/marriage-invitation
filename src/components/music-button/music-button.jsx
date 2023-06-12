c201467a06e7b4401bc4b6a98f54b5592e55d35f
Darkpfeffer <darkpfeffer97@gmail.com>
2023-06-12 13:57:00 +0200

    music button is responsive

--- src/components/music-button/music-button.jsx
+++ src/components/music-button/music-button.jsx
@@ -1,29 +1,57 @@
 //Importing
-import React from "react"
+import React, { useState, useEffect } from "react"
+
+//Import image
+import musicButton from "../../img/Music_Button.svg"
+import musicButtonActive from "../../img/Music_Button_Active.svg"
+
+//import self scss
+import "./music-button.scss"
+
+//audio player
+const useAudio = () => {
+    const [audio] = useState(new Audio("https://darkpfeffer-music-player.onrender.com/chapel-of-love.mp3"));
+    audio.volume = 1;
+    const [playing, setPlaying] = useState(false);
+
+    const toggle = () => setPlaying(!playing);
+
+    useEffect(() => {
+        playing ? audio.play() : audio.pause();
+    }, [playing])
+
+    useEffect(() => {
+        audio.addEventListener('ended', () => setPlaying(false));
+        return () => {
+            audio.removeEventListener('ended', () => setPlaying(false));
+        };
+    }, []);
+
+    return [playing, toggle]
+}
 
 export const MusicButton = () => {
-    const audio = new Audio("https://darkpfeffer-music-player.onrender.com/chapel-of-love.mp3")
-    let isPlaying = false;
-    audio.volume = 1;
-
-    const audioToggle = (event) => {
-        event.preventDefault()
-
-        if(isPlaying) {
-            audio.pause();
-            isPlaying = false;
-        } else {
-            audio.play();
-            isPlaying = true;
-        } 
-    }
-
+    const [playing, toggle] = useAudio()
+        
     return (
         <>
         <button
-            onClick={audioToggle}
-        >
-        Play
+            onClick={toggle}
+            className="music-button"
+        >{playing ? (
+            <img 
+                src={musicButtonActive} 
+                rel="active music button"
+                className="music-img" 
+            />
+        ) : (
+            <img 
+                src={musicButton} 
+                rel="music button" 
+                className="music-img"
+            />
+        )}
+        
         </button>
         </>
     )
